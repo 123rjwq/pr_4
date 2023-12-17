3639c84973d0d6e856f6b5e19842bbeadd72b2c9
123rjwq <145851361+123rjwq@users.noreply.github.com>
2023-12-18 02:55:39 +0300

    Add files via upload

---
+++ pr_4_1/pr_4_1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите третье число: ");
+        int number3 = int.Parse(Console.ReadLine());
+
+        int maxOfTwo = GetMax(number1, number2);
+        // Вызываем метод, передавая два первых числа
+        int maxOfThree = GetMax(maxOfTwo, number3);
+        // Вызываем метод, передавая результат и третье число
+
+        Console.WriteLine("Максимальное число: " + maxOfThree);
+    }
+
+    static int GetMax(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
+
+
---
+++ pr_4_2/pr_4_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = 10;
+        // Инициализация первого числа
+        int number2 = 20;
+        // Инициализация второго числа
+
+        Console.WriteLine("Исходные значения:");
+        Console.WriteLine("number1 = " + number1);
+        // Вывод исходного значения первого числа
+        Console.WriteLine("number2 = " + number2);
+        // Вывод исходного значения второго числа
+
+        SwapValues(ref number1, ref number2);
+        // Вызов метода SwapValues с передачей переменных по ссылке
+
+        Console.WriteLine("Значения после замены:");
+        Console.WriteLine("number1 = " + number1);
+        // Вывод значения первого числа после замены
+        Console.WriteLine("number2 = " + number2);
+        // Вывод значения второго числа после замены
+    }
+
+    static void SwapValues(ref int a, ref int b)
+    {
+        int temp = a;
+        // Создание временной переменной и сохранение значения первого числа в ней
+        a = b;
+        // Первому числу присваивается значение второго числа
+        b = temp;
+        // Второму числу присваивается значение из временной переменной
+    }
+}
+
